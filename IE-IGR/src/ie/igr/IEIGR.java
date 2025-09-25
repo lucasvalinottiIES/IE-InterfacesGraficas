15d129f55f9209884528f268991c3ed760161f72
lucasvalinottiIES <164922438+lucasvalinottiIES@users.noreply.github.com>
2025-09-25 09:25:42 -0300

    Revert "Update Personaje"
    This reverts commit d93f08e2165d6d35f899f4e157dc3fa8cd6b71e7.

--- IE-IGR/src/ie/igr/IEIGR.java
+++ IE-IGR/src/ie/igr/IEIGR.java
@@ -4,38 +4,17 @@
  */
 package ie.igr;
 
-
+/**
+ *
+ * @author Usuario
+ */
 public class IEIGR {
 
-    
-public abstract class Personaje {
-
-    public String nombre;
-    public int vida;
-    public int fuerza;
-    public int bendicion;
-    public String arma;
-
-    public Personaje(String nombre, int vida, int fuerza, int bendicion,String arma) {
-        this.nombre = nombre;
-        this.vida = vida;
-        this.fuerza = fuerza;
-        this.bendicion = bendicion;
-        this.arma = arma ;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
     }
-
-    public boolean estaVivo(Personaje personaje) {
-        return personaje.vida > 0;
-    }
-
-    public void atacar(Personaje personajeEnemigo) {
-        boolean estaVivo = this.estaVivo(personajeEnemigo);
-        if (estaVivo) {
-            personajeEnemigo.vida = personajeEnemigo.vida - this.fuerza;
-        }
-    }
-
-    
-}
     
 }
