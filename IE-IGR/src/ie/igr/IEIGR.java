28922cbf6e4306acd73ac81bff9a509408f3ab53
lucasvalinottiIES <164922438+lucasvalinottiIES@users.noreply.github.com>
2025-09-27 18:07:12 -0300

    ADD Logica Batalla

--- IE-IGR/src/ie/igr/IEIGR.java
+++ IE-IGR/src/ie/igr/IEIGR.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ie.igr;
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
 /**
  *
@@ -13,8 +15,45 @@ public class IEIGR {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+        int saludHeroe = validarInput("Salud Heroe");
+        int defensaHeroe = validarInput("Defensa Heroe");
+        int ataqueHeroe = validarInput("Ataque Heroe");
+        int saludVillano = validarInput("Salud Villano");
+        int defensaVillano = validarInput("Defensa Villano");
+        int ataqueVillano = validarInput("Ataque Villano");
+
+        Heroe heroe = new Heroe(saludHeroe, defensaHeroe, ataqueHeroe);
+        Villano villano = new Villano(saludVillano, defensaVillano, ataqueVillano);
+
+        System.out.println("Apodo Heroe");
+        heroe.ingresarApodo();
+        System.out.println("Apodo Villano");
+        villano.ingresarApodo();
+
+        Batalla batalla = new Batalla(heroe, villano);
+        batalla.mostrarEstado();
+        batalla.iniciarBatalla();
+    }
+
+    public static int validarInput(String atributo) {
+        Scanner sc = new Scanner(System.in);
+        int numero = 0;
+        boolean valido = false;
+
+        while (!valido) {
+            try {
+                System.out.print("Ingrese " + atributo + ":");
+                numero = sc.nextInt();
+                valido = true; 
+            } catch (InputMismatchException e) {
+                System.out.println("Error: debe ingresar un número entero.");
+                sc.nextLine(); 
+            }
+        }
+
+        return numero;
+    }
+
 }
